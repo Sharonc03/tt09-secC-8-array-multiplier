cebb8a4ca56c313a7a90fea825eab402b30fc6eb
Annie Huang <ah6478@nyu.edu>
2024-10-28 23:04:18 -0400

    checking

--- src/project.v
+++ src/project.v
@@ -22,28 +22,25 @@ module tt_um_C8_array_mult (
   wire [3:0] pp0, pp1, pp2, pp3;
   wire [7:0] sum0, sum1, sum2; 
   wire [7:0] shifted_pp1, shifted_pp2, shifted_pp3;
-  // All output pins must be assigned. If not used, assign to 0.
+ 
 
 
-    assign pp0 = m & {4{q[0]}};  
+  assign pp0 = m & {4{q[0]}};  
   assign pp1 = m & {4{q[1]}};  
   assign pp2 = m & {4{q[2]}};  
   assign pp3 = m & {4{q[3]}};      
 
-  // Shift partial products
   assign shifted_pp1 = {pp1, 1'b0};       
   assign shifted_pp2 = {pp2, 2'b00};       
   assign shifted_pp3 = {pp3, 3'b000};      
 
-  // Summing partial products
   assign sum0 = {4'b0000, pp0};              
   assign sum1 = sum0 + shifted_pp1;          
   assign sum2 = sum1 + shifted_pp2;          
   
-  // Final output
-  assign uo_out[7:0] = sum2 + shifted_pp3;    
-
-  // Assign unused IOs to avoid warnings
+  assign uo_out[7:0] = sum2 + shifted_pp3; 
+   
+ // All output pins must be assigned. If not used, assign to 0.
   assign uio_out = 8'b0;
   assign uio_oe = 8'b0;
 
