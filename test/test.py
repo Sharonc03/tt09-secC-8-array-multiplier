1eb8628e8763326d33f405989cc0dbb7dd63f9ec
Annie Huang <ah6478@nyu.edu>
2024-11-01 04:06:53 -0400

    test cases

--- test/test.py
+++ test/test.py
@@ -40,7 +40,7 @@ async def test_project(dut):
     # one or more clock cycles, and asserting the expected output values.
     
     # Set the input values you want to test #2
-    # m = 4'b0001  q = 4'b0001  p_ref = 8'b00000001;
+    # m = 4'b0010  q = 4'b0010  p_ref = 8'b00000100;
     dut.ui_in.value = 0b0010_0010
 
     # Wait for one clock cycle to see the output values
@@ -51,7 +51,7 @@ async def test_project(dut):
     assert dut.uo_out.value == 4
 
     # Set the input values you want to test #3
-    # m = 4'b0001  q = 4'b0001  p_ref = 8'b00000001;
+    # m = 4'b0011  q = 4'b0011  p_ref = 8'b00001001;
     dut.ui_in.value = 0b0011_0011
 
     # Wait for one clock cycle to see the output values
@@ -60,3 +60,26 @@ async def test_project(dut):
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
     assert dut.uo_out.value == 9
+
+
+    # Set the input values you want to test #4
+    # m = 4'b0101  q = 4'b0110  p_ref = 8'b00011110;
+    dut.ui_in.value = 0b0101_0110
+
+    # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+    assert dut.uo_out.value == 30
+
+    # Set the input values you want to test #5
+    # m = 4'b0111  q = 4'b0111  p_ref = 8'b00110001;
+    dut.ui_in.value = 0b0111_0111
+
+    # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+    assert dut.uo_out.value == 49
